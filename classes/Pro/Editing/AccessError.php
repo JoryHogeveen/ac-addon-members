3107fb4a142030471e67111bfa4b3994c2e3f3cf
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2017-10-21 16:26:52 +0200

    Nope.

--- classes/Pro/Editing/AccessError.php
+++ classes/Pro/Editing/AccessError.php
@@ -15,16 +15,6 @@ class ACA_Members_Pro_Editing_AccessError extends ACP_Editing_Model
 	}
 
 	/**
-	 * Get editing options when using an ajax callback
-	 *
-	 * @inheritdoc
-	 * @see  ACP_Editing_Model::get_ajax_options()
-	 */
-	public function get_ajax_options( $request ) {
-		return array_keys( wp_roles()->roles );
-	}
-
-	/**
 	 * @inheritdoc
 	 * @see  ACP_Editing_Model::get_view_settings()
 	 */
